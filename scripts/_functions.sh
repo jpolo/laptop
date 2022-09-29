7388caa061a630c0dc9e41791cb8cae40236f909
Julien Polo <julien.polo@captive.fr>
2022-09-29 15:53:08 +0200

    ✨ Load brew after installation

--- scripts/_functions.sh
+++ scripts/_functions.sh
@@ -64,6 +64,7 @@ install_ansible_macos()
   # Install Homebrew
   if ! [ -x "$(command -v brew)" ]; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    eval "$(/opt/homebrew/bin/brew shellenv)"
   fi
 
   check_ansible || brew install ansible
