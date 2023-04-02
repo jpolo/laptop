ba99ecfc73c765a930b8a4ec40f0beab3690fe09
Julien Polo <julien.polo@gmail.com>
2023-04-02 14:23:10 +0200

    ♻️ Move roles to main.sh #2

--- src/main.sh
+++ src/main.sh
@@ -8,15 +8,21 @@ ROOT_DIR=$(pwd)
 bootstrap
 
 # Install programs
+ensure_package "android-studio"
+ensure_package "chromedriver"
+ensure_package "discord"
 ensure_package "docker"
 ensure_package "drawio"
 ensure_package "git"
+ensure_package "google-chrome"
 ensure_package "gpg"
+ensure_package "iterm2"
 ensure_package "macpass"
 ensure_package "mercurial"
 ensure_package "notion"
 ensure_package "postman"
 ensure_package "rclone"
 ensure_package "slack"
+ensure_package "tfenv"
 ensure_package "watchman"
 
