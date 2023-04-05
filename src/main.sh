146e399c4baa669c9b5623b39aef906543f93c7a
Julien Polo <julien.polo@gmail.com>
2023-04-05 12:22:45 +0200

    🐛 Fix missing method

--- src/main.sh
+++ src/main.sh
@@ -3,8 +3,8 @@
 SCRIPT_DIR="$(dirname "$0")"
 source "$SCRIPT_DIR/_functions.sh"
 
-_laptop-execute bash "$LAPTOP_SOURCE_DIR/0-bootstrap.sh"
-_laptop-execute zsh  "$LAPTOP_SOURCE_DIR/1-configure-shell.zsh"
-_laptop-execute zsh "$LAPTOP_SOURCE_DIR/2-configure-all.zsh"
+_laptop_execute bash "$LAPTOP_SOURCE_DIR/0-bootstrap.sh"
+_laptop_execute zsh  "$LAPTOP_SOURCE_DIR/1-configure-shell.zsh"
+_laptop_execute zsh "$LAPTOP_SOURCE_DIR/2-configure-all.zsh"
 
 echo -e "${COLOR_WARNING}Warning: ZSH configuration was potentially modified, please close/open a new terminal to see changes.${NORMAL}"
