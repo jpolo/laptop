08db48c48d96e858dc16346b66facfebfa8d9c1a
Julien Polo <julien.polo@gmail.com>
2023-04-06 11:52:33 +0200

    ♻️ Refactor messages

--- src/_functions.sh
+++ src/_functions.sh
@@ -37,44 +37,6 @@ COLOR_INFO='\033[32m'
 export HOMEBREW_NO_INSTALL_CLEANUP=true
 export HOMEBREW_NO_ENV_HINTS=true
 
-log_info_msg() {
-  echo -n -e "${@}"
-  return 0
-}
-
-log_success_msg() {
-  echo -n -e "${@}"
-  echo -e "${SET_COL}${BRACKET}[${SUCCESS}  OK  ${BRACKET}]${NORMAL}"
-  return 0
-}
-
-log_failure_msg() {
-  echo -n -e "${@}"
-  echo -e "${SET_COL}${BRACKET}[${FAILURE} FAIL ${BRACKET}]${NORMAL}"
-  return 0
-}
-
-log_pass_msg() {
-  echo -n -e "${@}"
-  echo -e "${SET_COL}${BRACKET}[${NORMAL} PASS ${BRACKET}]${NORMAL}"
-  return 0
-}
-
-log_exec() {
-  local command=$@
-  local output;
-  { output=$( { { "$@" ; } 1>&3 ; } 2>&1); } 3>&1
-  local exit_code=$?
-
-  if [ "$exit_code" = "0" ]; then
-    log_success_msg
-  else
-    log_failure_msg $exit_code
-    eerror Command failed \
-      "\\n|  > $@" \
-      "\\n|  $output"
-  fi
-}
 
 is_arm() {
   test arm64 = $(uname -m)
@@ -115,12 +77,12 @@ ensure_package() {
   local package=${2:-$executable}
   local installation_message="- Ensure $executable"
 
-  log_info_msg "$installation_message"
+  _laptop_step_start "$installation_message"
   if [ $LAPTOP_PACKAGE_MANAGER = "brew" ];then
     if brew list $1 &>/dev/null; then
-      log_success_msg
+      _laptop_step_ok
     else
-      log_exec brew install $package --quiet
+      _laptop_step_exec brew install $package --quiet
     fi
   fi
 }
@@ -128,12 +90,12 @@ ensure_package() {
 ensure_asdf_plugin() {
   local name="$1"
   local url="$2"
-  log_info_msg "- Ensure asdf plugin $name"
+  _laptop_step_start "- Ensure asdf plugin $name"
 
   if ! asdf plugin-list | grep -Fq "$name"; then
-    log_exec asdf plugin-add "$name" "$url"
+    _laptop_step_exec asdf plugin-add "$name" "$url"
   else
-    log_success_msg
+    _laptop_step_ok
   fi
 }
 
@@ -141,13 +103,13 @@ ensure_asdf_language() {
   local language="$1"
   local version=$2 || "latest"
 
-  log_info_msg "- Ensure asdf $language $version"
+  _laptop_step_start "- Ensure asdf $language $version"
   if ! asdf list "$language" &>/dev/null; then
-    log_exec \
+    _laptop_step_exec \
       asdf install "$language" "$version" && \
       asdf global "$language" "$version"
   else
-    log_success_msg
+    _laptop_step_ok
   fi
 }
 
@@ -155,16 +117,16 @@ ensure_git_config() {
   local name="$1"
   local value="$2"
 
-  log_info_msg "- Ensure git config $name=$value"
+  _laptop_step_start "- Ensure git config $name=$value"
   if [ -z "$(git config --global $name)" ]; then
     if [ -z "${value}" ]; then
       echo "Git: Please enter value for '$name'"
       read value
     fi
 
-    log_exec git config --global $name $value
+    _laptop_step_exec git config --global $name $value
   else
-    log_success_msg
+    _laptop_step_ok
   fi
 }
 
@@ -172,14 +134,14 @@ ensure_ssh_key() {
   local ssh_key=~/.ssh/id_ed25519
   local email=$(git config --global user.email)
 
-  log_info_msg "- Ensure SSH key $ssh_key"
+  _laptop_step_start "- Ensure SSH key $ssh_key"
   if [ -z "$email" ];then
-    log_failure_msg
+    _laptop_step_fail
     eerror "git config user.email is empty";
   elif ! [ -f "$ssh_key" ]; then
-    log_exec ssh-keygen -t ed25519 -C "$email" -N '' -o -f $ssh_key
+    _laptop_step_exec ssh-keygen -t ed25519 -C "$email" -N '' -o -f $ssh_key
   else
-    log_success_msg
+    _laptop_step_ok
   fi
 }
 
@@ -187,29 +149,29 @@ ensure_defaults_bool() {
   local domain="$1"
   local key="$2"
   local value="$3"
-  log_info_msg "- Ensure defaults $domain $key=$value"
+  _laptop_step_start "- Ensure defaults $domain $key=$value"
   if command_exists "defaults"; then
-    log_exec defaults write -g $domain $key -bool $value;
+    _laptop_step_exec defaults write -g $domain $key -bool $value;
   else
-    log_pass_msg
+    _laptop_step_pass
   fi
 }
 
 ensure_directory() {
   local directory="$1"
-  log_info_msg "- Ensure directory $directory"
+  _laptop_step_start "- Ensure directory $directory"
   if [ ! -d $directory ]; then
-    log_exec mkdir -p $directory
+    _laptop_step_exec mkdir -p $directory
   else
-    log_success_msg
+    _laptop_step_ok
   fi
 }
 
 ensure_file() {
   local file_path="$1"
-  log_info_msg "- Ensure file $file_path"
+  _laptop_step_start "- Ensure file $file_path"
 
-  log_exec \
+  _laptop_step_exec \
     mkdir -p $(dirname $file_path) && \
     touch "$file_path"
 }
@@ -217,57 +179,56 @@ ensure_file() {
 ensure_file_template() {
   local template="$1"
   local target="$2"
-  local message="- Ensure file $target"
-
-  log_exec \
+  _laptop_step_start "- Ensure file $target"
+  _laptop_step_exec \
     mkdir -p $(dirname $target)
     cp "$LAPTOP_TEMPLATE_DIR/$template" "$target"
 }
 
 _laptop_ensure_rosetta2() {
   # Install Rosetta
-  log_info_msg "- Ensure Rosetta 2"
+  _laptop_step_start "- Ensure Rosetta 2"
   if is_arm && ! test -f /Library/Apple/usr/share/rosetta/rosetta; then
-    log_exec sudo softwareupdate --install-rosetta  --agree-to-license
+    _laptop_step_exec sudo softwareupdate --install-rosetta  --agree-to-license
   else
-    log_success_msg
+    _laptop_step_ok
   fi
 }
 
 _laptop_ensure_brew() {
   # Install Homebrew
   local brew_present=$(env -i zsh --login -c 'command -v brew');
-  log_info_msg "- Ensure Brew"
+  _laptop_step_start "- Ensure Brew"
 
   if [ -z "$brew_present" ]; then
-    log_exec /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    _laptop_step_exec /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     # eval "$(/opt/homebrew/bin/brew shellenv)" && \;
   else
-    log_success_msg
+    _laptop_step_ok
   fi
 }
 
 _laptop_ensure_zsh() {
-  log_info_msg "- Ensure ZSH as shell"
+  _laptop_step_start "- Ensure ZSH as shell"
 
   case "$SHELL" in
   */zsh)
-    log_success_msg
+    _laptop_step_ok
     ;;
   *)
     local shell_path="$(command -v zsh)"
-    log_exec sudo chsh -s "$shell_path" "$USER"
+    _laptop_step_exec sudo chsh -s "$shell_path" "$USER"
     ;;
   esac
 }
 
 _laptop_ensure_xcode() {
   # Install XCode
-  log_info_msg "- Ensure Build tools"
+  _laptop_step_start "- Ensure Build tools"
   if ! [ -x "$(command -v gcc)" ]; then
-    log_exec xcode-select --install;
+    _laptop_step_exec xcode-select --install;
   else
-    log_success_msg
+    _laptop_step_ok
   fi
 }
 
@@ -300,6 +261,41 @@ _laptop_shell() {
   env -i $shell --login $script
 }
 
+_laptop_step_start() {
+  echo -n -e "${@}"
+  return 0
+}
 
+_laptop_step_ok() {
+  # echo -n -e "${@}"
+  echo -e "${SET_COL}${BRACKET}[${SUCCESS}  OK  ${BRACKET}]${NORMAL}"
+  return 0
+}
 
+_laptop_step_fail() {
+  # echo -n -e "${@}"
+  echo -e "${SET_COL}${BRACKET}[${FAILURE} FAIL ${BRACKET}]${NORMAL}"
+  return 0
+}
 
+_laptop_step_pass() {
+  #echo -n -e "${@}"
+  echo -e "${SET_COL}${BRACKET}[${NORMAL} PASS ${BRACKET}]${NORMAL}"
+  return 0
+}
+
+_laptop_step_exec() {
+  local command=$@
+  local output;
+  { output=$( { { "$@" ; } 1>&3 ; } 2>&1); } 3>&1
+  local exit_code=$?
+
+  if [ "$exit_code" = "0" ]; then
+    _laptop_step_ok
+  else
+    _laptop_step_fail $exit_code
+    eerror Command failed \
+      "\\n|  > $@" \
+      "\\n|  $output"
+  fi
+}
