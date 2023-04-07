9f1a0e3094f0308757108a6792a6d7b6f7fd04b6
Julien Polo <julien.polo@gmail.com>
2023-04-07 21:13:44 +0200

    ♻️ Remove bootstrap

--- src/main.sh
+++ src/main.sh
@@ -3,8 +3,13 @@
 SCRIPT_DIR="$(dirname "$0")"
 source "$SCRIPT_DIR/_functions.sh"
 
-_laptop_shell bash "$LAPTOP_SOURCE_DIR/0-bootstrap.sh"
-_laptop_shell zsh  "$LAPTOP_SOURCE_DIR/1-configure-shell.zsh"
-_laptop_shell zsh "$LAPTOP_SOURCE_DIR/2-configure-all.zsh"
+# Bootstrap
+_laptop_bootstrap
+ensure_file_template "zprofile" ~/.zprofile
+ensure_file_template "zshrc" ~/.zshrc
+
+# Installation
+_laptop_shell zsh  "$LAPTOP_SOURCE_DIR/0-configure-shell.zsh"
+_laptop_shell zsh "$LAPTOP_SOURCE_DIR/1-configure-all.zsh"
 
 einfo "ZSH configuration was potentially modified, please close/open a new terminal to see changes."
