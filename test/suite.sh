bacb7b113dad17c5d3fb23ea7a43468497146f57
Julien Polo <julien.polo@gmail.com>
2023-05-24 21:44:19 +0200

    ✅ Add test for ensure_asdf_plugin

--- test/suite.sh
+++ test/suite.sh
@@ -10,6 +10,14 @@ cd "test_data"
 # Try to run brew installation
 _laptop_ensure_brew
 
+# Test ensure_package
+ensure_package "asdf"
+ensure_package "asdf"
+
+# Test ensure_asdf_plugin
+ensure_asdf_plugin "nodejs"
+ensure_asdf_plugin "nodejs"
+
 # Test ensure_directory
 ensure_directory "./folder_create/"
 ensure_directory "./folder_create/"
@@ -17,7 +25,3 @@ ensure_directory "./folder_create/"
 # Test ensure_file
 ensure_file "./test_file_create"
 ensure_file "./test_file_create"
-
-# Test ensure_package
-ensure_package "coreutils"
-ensure_package "coreutils"
