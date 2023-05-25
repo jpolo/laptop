1eb037ba9d037d191675369cc43cfd7fdcf2db19
Julien Polo <julien.polo@gmail.com>
2023-05-25 19:08:04 +0200

    ♻️ Improve ensure_vscode_setting quoting

--- src/_functions.sh
+++ src/_functions.sh
@@ -224,8 +224,7 @@ ensure_vscode_setting() {
   local json_path="$1"
   local json_value="$2"
   local vscode_settings_file=""
-  local jsonc=(npm exec --package=jsonc-cli -- jsonc);
-  local jsonc_args=(-v "$json_value")
+  local jsonc_args="-v $(quote "$json_value")"
   if [ "$json_value" = "" ]; then
     jsonc_args=("--delete")
   fi
@@ -245,8 +244,8 @@ ensure_vscode_setting() {
 
   _laptop_step_start "- Ensure VSCode Setting $json_path=$json_value"
   _laptop_step_eval "\
-  cat "$vscode_settings_file" | \
-  ${jsonc[@]} modify -n -m -p "$json_path" ${jsonc_args[@]} -f "$vscode_settings_file"
+  cat $(quote $vscode_settings_file) | \
+  jsonc modify -n -m -p $(quote $json_path) $jsonc_args -f $(quote $vscode_settings_file) \
   "
 }
 
