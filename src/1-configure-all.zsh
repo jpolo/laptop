1d23563107e1a633c44c175f24cb80739a98abd5
Julien Polo <julien.polo@gmail.com>
2023-05-27 21:40:41 +0200

    ✨ Add findutils, gnutls, pv

--- src/1-configure-all.zsh
+++ src/1-configure-all.zsh
@@ -9,14 +9,6 @@ ensure_directory ~/Code
 # Configure git
 ensure_file "$XDG_CONFIG_HOME/git/config"
 
-ensure_package "git"
-# https://pawelgrzybek.com/auto-setup-remote-branch-and-never-again-see-an-error-about-the-missing-upstream/
-ensure_git_config "push.default" "current"
-ensure_git_config "push.autoSetupRemote" "true"
-ensure_git_config "fetch.prune" "true"
-ensure_git_config "user.email"
-ensure_git_config "user.name"
-
 ensure_ssh_key
 
 # Default settings
@@ -29,48 +21,64 @@ ensure_defaults_bool "" NSAutomaticSpellingCorrectionEnabled false
 ensure_defaults_bool "" NSAutomaticTextCompletionEnabled false
 # ensure_defaults_bool "/Library/Preferences/com.apple.commerce.plist" AutoUpdate false
 
+# Install standard utils
+ensure_package "coreutils"
+ensure_package "moreutils"
+ensure_package "findutils"
+ensure_package "git"
+ensure_package "gnutls"
+ensure_package "mercurial"
+
+# https://pawelgrzybek.com/auto-setup-remote-branch-and-never-again-see-an-error-about-the-missing-upstream/
+ensure_git_config "push.default" "current"
+ensure_git_config "push.autoSetupRemote" "true"
+ensure_git_config "fetch.prune" "true"
+ensure_git_config "user.email"
+ensure_git_config "user.name"
+
 # Install library
 ensure_package "graphviz"
+ensure_package "imagemagick"
 ensure_package "libpq"
 ensure_package "libyaml"
 ensure_package "openssl"
 ensure_package "vips"
 
-# Install programs
-ensure_package "android-studio"
+# Install CLI tools
 ensure_package "asdf"
-ensure_package "chromedriver"
-ensure_package "coreutils"
-ensure_package "discord"
-ensure_package "docker"
-ensure_package "drawio"
-ensure_package "flipper"
-ensure_package "google-chrome"
-ensure_package "google-drive"
 ensure_package "gh"
 ensure_package "gpg"
-ensure_package "imagemagick"
-ensure_package "iterm2"
 ensure_package "jq"
-ensure_package "macpass"
-ensure_package "mercurial"
-ensure_package "moreutils"
-ensure_package "notion"
-ensure_package "postman"
-ensure_package "rectangle"
+ensure_package "pv"
 ensure_package "rclone"
-ensure_package "slack"
 ensure_package "tfenv"
-# FIXME: Does not work on Apple Silicon M1, M2, etc
-# Error: Cask virtualbox depends on hardware architecture being one of [{:type=>:intel, :bits=>64}], but you are running {:type=>:arm, :bits=>64}.
-# ensure_package "virtualbox"
-ensure_package "visual-studio-code"
+ensure_package "tmux"
 ensure_package "universal-ctags"
 ensure_package "watchman"
 ensure_package "webp"
 ensure_package "wget"
 ensure_package "yarn"
 ensure_package "yq"
+
+# Install programs
+ensure_package "android-studio"
+ensure_package "chromedriver"
+ensure_package "discord"
+ensure_package "docker"
+ensure_package "drawio"
+ensure_package "flipper"
+ensure_package "google-chrome"
+ensure_package "google-drive"
+ensure_package "iterm2"
+ensure_package "macpass"
+ensure_package "notion"
+ensure_package "postman"
+ensure_package "rectangle"
+ensure_package "slack"
+# FIXME: Does not work on Apple Silicon M1, M2, etc
+# Error: Cask virtualbox depends on hardware architecture being one of [{:type=>:intel, :bits=>64}], but you are running {:type=>:arm, :bits=>64}.
+# ensure_package "virtualbox"
+ensure_package "visual-studio-code"
 
 # Install ASDF plugins
 ensure_asdf_plugin "java" "https://github.com/halcyon/asdf-java.git"
