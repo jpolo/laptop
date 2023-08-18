93332aa545300461c60e0c2c9629f94a96b569bf
Julien Polo <julien.polo@gmail.com>
2023-08-18 14:34:42 +0200

    ✨ Add recipe for libpq and libyaml

---
+++ src/_recipes.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+ensure_package__libpq() {
+  if [ $LAPTOP_PACKAGE_MANAGER = "brew" ];then
+    ensure_package_default "libpq"
+  else
+    ensure_package_default "libpq-dev"
+  fi
+}
+
+
+ensure_package__libyaml() {
+  if [ $LAPTOP_PACKAGE_MANAGER = "brew" ];then
+    ensure_package_default "libyaml"
+  else
+    ensure_package_default "libyaml-dev"
+  fi
+}
