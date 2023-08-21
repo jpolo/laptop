40c49a2317e24bdf595c5170261b908b978c1ed1
Julien Polo <julien.polo@captive.fr>
2023-08-21 08:54:08 +0000

    ✨ Add pack:core and pack:utils

--- src/_recipes.sh
+++ src/_recipes.sh
@@ -46,3 +46,46 @@ ensure_package__libvips() {
     ensure_package_default "libvips-dev"
   fi
 }
+
+ensure_package__pack:core() {
+  # A pack of required packages
+
+  # Install core tools
+  ensure_package "coreutils"
+  ensure_package "moreutils"
+  ensure_package "findutils"
+  ensure_package "asdf"
+  ensure_package "curl"
+  ensure_package "git"
+  ensure_package "gnutls"
+  ensure_package "gnupg"
+  ensure_package "mercurial"
+  ensure_package "gpg"
+  ensure_package "jq"
+  ensure_package "rclone"
+  ensure_package "webp"
+  ensure_package "wget"
+  ensure_package "yarn"
+
+  # Install library
+  ensure_package "graphviz"
+  ensure_package "imagemagick"
+  ensure_package "libpq"
+  ensure_package "libyaml"
+  ensure_package "libvips"
+  ensure_package "openssl"
+}
+
+ensure_package__pack:utils() {
+  # A pack of useful tools
+  ensure_package "adr-tools"
+  ensure_package "pv"
+  ensure_package "tmux"
+  ensure_package "tree"
+  ensure_package "fzf"
+  ensure_package "gh"
+  ensure_package "watchman"
+  # ensure_package "trash"
+  ensure_package "universal-ctags"
+  ensure_package "yq"
+}
