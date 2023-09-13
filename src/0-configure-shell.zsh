25b2364ea48f0ff1166cd576f18d5650253c103d
Julien Polo <julien.polo@gmail.com>
2023-09-13 15:55:32 +0200

    🐛 Avoid overwriting personal files

--- src/0-configure-shell.zsh
+++ src/0-configure-shell.zsh
@@ -5,5 +5,9 @@ source "$SCRIPT_DIR/_functions.sh"
 
 # Ensure ZSH Configuration
 ensure_file_template "zshrc.d/00_init.sh" "$XDG_DATA_HOME/zsh/00_init.sh"
-ensure_file_template "zshrc.d/01_custom.sh" "$XDG_DATA_HOME/zsh/01_custom.sh" -n
-ensure_file_template "p10k.zsh" "${POWERLEVEL9K_CONFIG_FILE:-$XDG_CONFIG_HOME/zsh/p10k.zsh}" -n
+if [ ! -f "$XDG_DATA_HOME/zsh/01_custom.sh" ];then
+  ensure_file_template "zshrc.d/01_custom.sh" "$XDG_DATA_HOME/zsh/01_custom.sh"
+fi
+if [ ! -f "${POWERLEVEL9K_CONFIG_FILE:-$XDG_CONFIG_HOME/zsh/p10k.zsh}" ];then
+  ensure_file_template "p10k.zsh" "${POWERLEVEL9K_CONFIG_FILE:-$XDG_CONFIG_HOME/zsh/p10k.zsh}"
+fi
