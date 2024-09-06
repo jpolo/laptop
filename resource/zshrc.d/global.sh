b9971d416a658087de00afe799b5dc893fe31275
Julien Polo <julien.polo@gmail.com>
2024-09-06 18:27:10 +0200

    🐛 Fix zi fallback

--- resource/zshrc.d/global.sh
+++ resource/zshrc.d/global.sh
@@ -13,63 +13,56 @@ if [[ -f "$POWERLEVEL9K_CONFIG_FILE" ]]; then
   source "$POWERLEVEL9K_CONFIG_FILE"
 fi
 
-if ! command -v zi &> /dev/null; then
-  _ZI_FALLBACK=1
-  zi() {
-  }
+if command -v zi &> /dev/null; then
+  # When zi is available
+  zi ice depth=1
+  zi light romkatv/powerlevel10k
+
+  # zi ice nocompile:! pick:c.zsh atpull:%atclone atclone:'dircolors -b LS_COLORS > c.zsh'
+  # zi light trapd00r/LS_COLORS
+
+  # Fish like suggestions
+  zi ice wait"0a" lucid atload"_zsh_autosuggest_start"
+  zi light zsh-users/zsh-autosuggestions
+
+  zi ice wait:0a lucid atload'bindkey "$terminfo[kcuu1]" history-substring-search-up; bindkey "$terminfo[kcud1]" history-substring-search-down'
+  zi light zsh-users/zsh-history-substring-search
+
+  ##
+  # Completions
+  ##
+
+  # Brew completions
+  zi ice wait"0b" lucid blockf
+  # Brew completions
+  if type brew &>/dev/null; then
+    FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
+
+    autoload -Uz compinit
+    compinit
+  fi
+
+  # Zsh completions
+  zi light zsh-users/zsh-completions
+
+  # Zsh OMZ libraries
+  zi snippet OMZL::clipboard.zsh
+  zi snippet OMZL::compfix.zsh
+  zi snippet OMZL::correction.zsh
+  zi snippet OMZL::completion.zsh
+  zi snippet OMZL::grep.zsh
+  zi snippet OMZL::history.zsh
+  # zi snippet OMZL::spectrum.zsh
+
+  # Zsh OMZ plugins
+  zi snippet OMZP::asdf
+  # zi snippet OMZP::fzf
+
+  zi light hlissner/zsh-autopair
+
+  zi ice silent wait:0c atload"ZI[COMPINIT_OPTS]=-C; zpcompinit"
+  zi light zdharma-continuum/fast-syntax-highlighting
+
+  # Install also as a zsh plugin
+  zi light jpolo/laptop
 fi
-
-# When zi is available
-zi ice depth=1
-zi light romkatv/powerlevel10k
-
-# zi ice nocompile:! pick:c.zsh atpull:%atclone atclone:'dircolors -b LS_COLORS > c.zsh'
-# zi light trapd00r/LS_COLORS
-
-# Fish like suggestions
-zi ice wait"0a" lucid atload"_zsh_autosuggest_start"
-zi light zsh-users/zsh-autosuggestions
-
-zi ice wait:0a lucid atload'bindkey "$terminfo[kcuu1]" history-substring-search-up; bindkey "$terminfo[kcud1]" history-substring-search-down'
-zi light zsh-users/zsh-history-substring-search
-
-##
-# Completions
-##
-
-# Brew completions
-zi ice wait"0b" lucid blockf
-# Brew completions
-if type brew &>/dev/null; then
-  FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
-
-  autoload -Uz compinit
-  compinit
-fi
-
-# Zsh completions
-zi light zsh-users/zsh-completions
-
-# Zsh OMZ libraries
-zi snippet OMZL::clipboard.zsh
-zi snippet OMZL::compfix.zsh
-zi snippet OMZL::correction.zsh
-zi snippet OMZL::completion.zsh
-zi snippet OMZL::grep.zsh
-zi snippet OMZL::history.zsh
-# zi snippet OMZL::spectrum.zsh
-
-# Zsh OMZ plugins
-zi snippet OMZP::asdf
-# zi snippet OMZP::fzf
-
-zi light hlissner/zsh-autopair
-
-zi ice silent wait:0c atload"ZI[COMPINIT_OPTS]=-C; zpcompinit"
-zi light zdharma-continuum/fast-syntax-highlighting
-
-# Install also as a zsh plugin
-zi light jpolo/laptop
-
-# Cleanup fallback
-[ -z "$_ZI_FALLBACK" ] && unset -f zi
