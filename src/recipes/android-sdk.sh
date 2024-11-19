e27ae9bdae0adcb70055c4dd2a41ce10057ae7ee
Julien Polo <julien.polo@gmail.com>
2024-11-19 15:01:39 +0100

    ✨ Install android-commandlinetools

--- src/recipes/android-sdk.sh
+++ src/recipes/android-sdk.sh
@@ -3,8 +3,7 @@
 laptop::ensure_package__android-sdk() {
   if [ "$LAPTOP_PACKAGE_MANAGER" = "brew" ];then
     brew remove android-sdk &>/dev/null
-    brew remove android-commandlinetools &>/dev/null
-    # laptop::ensure_package_default "android-commandlinetools"
+    laptop::ensure_package_default "android-commandlinetools"
   else
     laptop::step_start "- Ensure android-sdk installed (via git)"
     laptop::step_eval "echo >&2 Not implemented;exit 1;"
