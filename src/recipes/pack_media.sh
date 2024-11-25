d46fc868e4bcc70716824f1d5e937184fdbccc57
Julien Polo <julien.polo@gmail.com>
2024-11-25 14:14:18 +0100

    ♻️ Add pack:media

---
+++ src/recipes/pack_media.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Pack to manipulate photo, videos and other media
+laptop::ensure_package__pack:media() {
+  laptop::ensure_package "inkscape"
+  laptop::ensure_package "gimp"
+  laptop::ensure_package "openshot-video-editor"
+}
