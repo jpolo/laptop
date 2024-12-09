208f53d8baf7437a9649d6fd8da67d6b78afd851
Julien Polo <julien.polo@gmail.com>
2024-12-09 11:36:58 +0100

    🐛 Fix version check
    Reason : before it only worked when running laptop from a current path containing laptop repository (so it was useless)

--- src/function/self_check_version.sh
+++ src/function/self_check_version.sh
@@ -2,8 +2,8 @@
 
 laptop::self_check_version() {
   if laptop::self_updated; then
-    laptop::warn "A new laptop version is available. Run 'laptop upgrade' to download it."
+    laptop::info "Laptop is up to date"
   else
-    laptop::info "✓ laptop is up to date"
+    laptop::warn "A new laptop version is available. Run 'laptop upgrade' to download it."
   fi
 }
--- src/function/self_updated.sh
+++ src/function/self_updated.sh
@@ -3,18 +3,19 @@
 laptop::self_updated() {
   local current_branch
   local behind_count
+
   if [[ -d "$LAPTOP_HOME/.git" ]]; then
     # laptop is managed with git
-    current_branch=$(git --work-tree="$LAPTOP_HOME" rev-parse --abbrev-ref HEAD 2>/dev/null)
+    current_branch=$(git -C "$LAPTOP_HOME" rev-parse --abbrev-ref HEAD 2>/dev/null)
 
     # Fetch updates from the remote
-    git --work-tree="$LAPTOP_HOME" --git-dir="$LAPTOP_HOME/.git" fetch origin >/dev/null 2>&1
+    git -C "$LAPTOP_HOME" --git-dir="$LAPTOP_HOME/.git" fetch origin >/dev/null 2>&1 || true
 
     # Check if the working directory is outdated
-    behind_count=$(git --work-tree="$LAPTOP_HOME" rev-list --left-right --count HEAD...origin/"$current_branch" 2>/dev/null | awk '{print $2}')
+    behind_count=$(git -C "$LAPTOP_HOME" rev-list --left-right --count HEAD...origin/"$current_branch" 2>/dev/null | awk '{print $2}')
 
     # If behind_count is greater than 0, the repo is outdated
-    if [ "$behind_count" -gt 0 ]; then
+    if [[ "$behind_count" -gt 0 ]]; then
       return 1 # Outdated
     fi
   fi
