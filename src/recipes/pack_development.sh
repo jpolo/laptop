579f7ab9affa00338a460cb6cd34566f4a79df1e
Julien Polo <julien.polo@gmail.com>
2025-02-01 22:45:06 +0100

    ✨ Add virtualbox

--- src/recipes/pack_development.sh
+++ src/recipes/pack_development.sh
@@ -9,9 +9,7 @@ laptop::ensure_package__pack:development() {
   laptop::ensure_package "mysql"
   laptop::ensure_package "pgadmin4"
   laptop::ensure_package "postman"
-  # FIXME: Does not work on Apple Silicon M1, M2, etc
-  # Error: Cask virtualbox depends on hardware architecture being one of [{:type=>:intel, :bits=>64}], but you are running {:type=>:arm, :bits=>64}.
-  # laptop::ensure_package "virtualbox"
+  laptop::ensure_package "virtualbox"
   laptop::ensure_package "visual-studio-code"
 
   # Fonts for development
