c0479ec6fe87a3803f1733b4b4becea625fd242e
Julien Polo <julien.polo@gmail.com>
2025-02-03 17:07:05 +0100

    ✨ Add xeol recipe

---
+++ src/recipes/xeol.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+#
+# Install xeol on your project / docker container. This script will install xeol on your system using the package manager you have selected.
+#
+# @see https://github.com/xeol-io/xeol
+#
+laptop::ensure_package__xeol() {
+  if [ "$LAPTOP_PACKAGE_MANAGER" = "brew" ]; then
+    laptop::ensure_brew_tap "xeol-io/xeol"
+    laptop::ensure_package_default "xeol"
+  else
+    laptop::ensure_package_default "xeol"
+  fi
+}
