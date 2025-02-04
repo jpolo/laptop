67e3b7d18789ba2186339728567cbe15fc2e7114
Julien Polo <julien.polo@gmail.com>
2025-02-04 14:55:53 +0100

    🐛 Fix find warning

--- src/function/source_all.sh
+++ src/function/source_all.sh
@@ -3,7 +3,7 @@
 laptop::source_all() {
   local script_dir="$1"
   # shellcheck disable=SC2044
-  for file in $(find "$script_dir" -type f -maxdepth 1 -name '*.sh' -print); do
+  for file in $(find "$script_dir" -maxdepth 1 -type f -name '*.sh' -print); do
     # shellcheck disable=SC1090
     source "$file"
   done
