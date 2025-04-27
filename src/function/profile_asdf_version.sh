762429cb1260f21fb989066cfc57ee440c112784
Julien Polo <julien.polo@gmail.com>
2025-04-27 17:56:57 +0200

    ♻️ Add laptop::profile_asdf_version

---
+++ src/function/profile_asdf_version.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Retrieves the version of a specified language from the .tool-versions file
+# in the current profile directory.
+#
+# Usage:
+#   laptop::profile_asdf_version <language>
+#
+# Example:
+#   version=$(laptop::profile_asdf_version "nodejs")
+laptop::profile_asdf_version() {
+  local language="$1"
+
+  # Path to the .tool-versions file
+  local tool_versions_file
+  tool_versions_file="$(laptop::profile_dir)/.tool-versions"
+
+  local target_language
+  local target_version
+
+  # Check if the file exists
+  if [[ -f "$tool_versions_file" ]]; then
+    # Read the file line by line
+    while IFS= read -r line; do
+      # Split the line into language and version
+      target_language=$(echo "$line" | awk '{print $1}')
+      target_version=$(echo "$line" | awk '{print $2}')
+
+      # If the language matches, return the version
+      if [[ "$target_language" == "$language" ]]; then
+        echo "$target_version"
+        return 0
+      fi
+    done < "$tool_versions_file"
+  fi
+}
