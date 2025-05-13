b5772ff834082067bba850044bad9cf0d8c1790b
Julien Polo <julien.polo@gmail.com>
2025-05-13 16:14:40 +0200

    ✨ Add ollama nomic-embed-text

--- src/recipes/pack_productivity.sh
+++ src/recipes/pack_productivity.sh
@@ -4,12 +4,13 @@ laptop::ensure_package__pack:productivity() {
   laptop::ensure_package "chatgpt"
   laptop::ensure_package "ollama"
   laptop::ensure_ollama_model "qwen2.5-coder:1.5b"
+  laptop::ensure_ollama_model "nomic-embed-text"
   laptop::ensure_package "drawio"
   laptop::ensure_package "notion"
   # laptop::ensure_package "rectangle"
   if [[ ${LAPTOP_PROFILE_PRIVACY:-strict} = strict ]];then
     laptop::ensure_package "vivaldi"
-  else 
+  else
     laptop::ensure_package "google-chrome"
     laptop::ensure_package "google-drive"
   fi
