498cf9d1891f73efe80b50dfd45e071ea5277bd5
Julien Polo <julien.polo@gmail.com>
2025-09-02 15:15:14 +0200

    ✨ Add excalidrawz

--- src/recipes/pack_media.sh
+++ src/recipes/pack_media.sh
@@ -2,6 +2,7 @@
 
 # Pack to manipulate photo, videos and other media
 laptop::ensure_package__pack:media() {
+  laptop::ensure_package "excalidrawz"
   laptop::ensure_package "inkscape"
   laptop::ensure_package "gimp"
   laptop::ensure_package "openshot-video-editor"
