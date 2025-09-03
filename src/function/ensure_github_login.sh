75160f69df081983a3a65de87517d9a054aec875
Julien Polo <julien.polo@gmail.com>
2025-09-03 12:21:24 +0200

    🚨 Fix SC2015

--- src/function/ensure_github_login.sh
+++ src/function/ensure_github_login.sh
@@ -3,9 +3,11 @@
 laptop::ensure_github_login() {
   laptop::step_start "- Ensure Github logged in"
   if ! gh auth status -h github.com &>/dev/null; then
-    gh auth login -p ssh -h github.com -w \
-      && eval "$(ssh-agent -s)" \
-      && laptop::step_ok || laptop::step_fail
+    if gh auth login -p ssh -h github.com -w && eval "$(ssh-agent -s)"; then
+      laptop::step_ok
+    else
+      laptop::step_fail
+    fi
   else
     laptop::step_ok
   fi
