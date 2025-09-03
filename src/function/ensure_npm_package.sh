6660a5acc00471c44da290c32b5c28a2fe548f9c
Julien Polo <julien.polo@gmail.com>
2025-09-03 12:16:48 +0200

    🚨 Fix SC2145

--- src/function/ensure_npm_package.sh
+++ src/function/ensure_npm_package.sh
@@ -4,11 +4,10 @@ laptop::ensure_npm_package() {
   local package="$1"
 
   laptop::step_start "- Ensure NPM package '$package'"
-  local npm_args=("--quiet --global")
 
   if [ -n "$(npm list --global --parseable "$package")" ]; then
     laptop::step_ok
   else
-    laptop::step_eval "npm install ${npm_args[@]} $(quote "$package")"
+    laptop::step_eval "npm install --quiet --global $(quote "$package")"
   fi
 }
