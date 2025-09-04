d87eaa462cbd2448d7e54cec1f965e398077091d
Julien Polo <julien.polo@gmail.com>
2025-09-04 09:52:34 +0200

    🐛 Install gcloud cli using cask

--- src/recipes/gcloud.sh
+++ src/recipes/gcloud.sh
@@ -2,7 +2,7 @@
 
 laptop::ensure_package__gcloud() {
   if [ "$LAPTOP_PACKAGE_MANAGER" = "brew" ]; then
-    laptop::ensure_package_default "gcloud-cli"
+    laptop::ensure_brew_cask_package "gcloud-cli"
   else
     laptop::ensure_package_default "gcloud"
   fi
