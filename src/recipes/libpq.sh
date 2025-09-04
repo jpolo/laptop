b320c97c23a622fb1990e3ae4cfe616f30ada706
Julien Polo <julien.polo@gmail.com>
2025-09-04 10:07:07 +0200

    ✨ Add brew link after libpq installation

--- src/recipes/libpq.sh
+++ src/recipes/libpq.sh
@@ -2,7 +2,8 @@
 
 laptop::ensure_package__libpq() {
   if [ "$LAPTOP_PACKAGE_MANAGER" = "brew" ]; then
-    laptop::ensure_package_default "libpq"
+    laptop::ensure_brew_package "libpq"
+    laptop::ensure_brew_link "libpq"
   else
     laptop::ensure_package_default "libpq-dev"
   fi
