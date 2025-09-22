a4a7aa8194f1ad3d2a95751e34c9bd17e0d07b7d
Julien <julien.polo@inthememory.com>
2025-09-22 17:36:48 +0200

    🔧 Update SSH config path variable for clarity and maintainability

--- src/function/ssh_ensure_directory.sh
+++ src/function/ssh_ensure_directory.sh
@@ -3,10 +3,11 @@
 laptop_ssh_ensure_directory() {
   local ssh_dir
   ssh_dir="$HOME/.ssh"
+  local ssh_config_file="$ssh_dir/config"
 
   laptop_step_start "- Ensure SSH directory '$ssh_dir'"
   laptop_step_eval "mkdir -p \"$ssh_dir\" && chmod 0700 \"$ssh_dir\""
 
-  laptop_step_start "- Ensure SSH config '$ssh_dir/config'"
-  laptop_step_eval "touch \"$ssh_dir/config\" && chmod 0600 \"$ssh_dir/config\""
+  laptop_step_start "- Ensure SSH config '$ssh_config_file'"
+  laptop_step_eval "touch \"$ssh_config_file\" && chmod 0600 \"$ssh_config_file\""
 }
