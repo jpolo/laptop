33e905935ae84b6ff63801b9ebff0efc83e3880b
Julien <julien.polo@inthememory.com>
2025-09-23 16:38:43 +0200

    🔧 Update SSH configuration settings to enhance key management and agent integration

--- profile/default/steps/1-configure-all.zsh
+++ profile/default/steps/1-configure-all.zsh
@@ -57,6 +57,10 @@ if [ -z "$LAPTOP_DEVCONTAINER" ];then
   laptop_github_ensure_login
   laptop_ssh_ensure_directory
   laptop_ssh_ensure_key "ed25519"
+  laptop_ssh_ensure_setting "Host *" "IdentityFile" "~/.ssh/id_ed25519"
+
+  laptop_ssh_ensure_setting "Host *" "AddKeysToAgent" "yes"
+  laptop_ssh_ensure_setting "Host *" "UseKeychain" "yes"
 fi
 
 # Install globally tools using asdf to the version specified in profile/${profile_name}/.tool-versions
