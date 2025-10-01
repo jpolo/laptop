137302d8e096af93c40a81f5f5683640d9d7bdae
Julien <julien.polo@inthememory.com>
2025-10-01 14:07:53 +0200

    ✨ Update file_ensure function to support status options for file presence management

--- src/function/file_ensure.sh
+++ src/function/file_ensure.sh
@@ -3,13 +3,29 @@
 # Ensure that a file exists, creating parent directories as needed.
 #
 # Usage:
-#   laptop_file_ensure <file_path>
+#   laptop_file_ensure <file_path> [--status present|absent]
+#
+# Options:
+#   --status present|absent
+#
 laptop_file_ensure() {
   local file_path="$1"
-  laptop_step_start "- Ensure file '$(laptop_path_print "$file_path")'"
+  # parse options
+  local package_status="present"
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+      -s|--status) package_status="$2"; shift 2;;
+      *) shift;;
+    esac
+  done
+  laptop_step_start "- Ensure file '$(laptop_path_print "$file_path")' is $package_status"
 
-  laptop_step_eval "\
+  if [ "$package_status" = "present" ]; then
+    laptop_step_eval "\
     mkdir -p $(quote "$(dirname "$file_path")") && \
     touch $(quote "$file_path")
     "
+  else
+    laptop_step_eval "rm -f $(quote "$file_path")"
+  fi
 }
