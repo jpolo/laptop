c3ca6815155633e73826736de73cc0ad729da423
Julien <julien.polo@inthememory.com>
2025-10-01 14:05:31 +0200

    ✨ Implement status options for plugin management functions to control installation and uninstallation behavior

--- src/function/asdf_ensure_package.sh
+++ src/function/asdf_ensure_package.sh
@@ -3,19 +3,35 @@
 # Install asdf `package` if not present
 #
 # Usage:
-#   laptop_asdf_ensure_package <package> <version>
+#   laptop_asdf_ensure_package <package> <version> [--status present|absent]
+#
+# Options:
+#   --status present|absent
 #
 laptop_asdf_ensure_package() {
   local package="$1"
   local version="${2:-latest}"
+  # parse options
+  local package_status="present"
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+      -s|--status) package_status="$2"; shift 2;;
+      *) shift;;
+    esac
+  done
 
-  laptop_step_start "- Ensure asdf '$package' '$version'"
-  if ! asdf list "$package" 2>/dev/null | grep -Fq "$version"; then
-    laptop_step_exec \
-      asdf install "$package" "$version" &&
-      asdf set --home "$package" "$version"
+  laptop_step_start "- Ensure asdf '$package' '$version' is $package_status"
+  if [ "$package_status" = "present" ]; then
+    if ! asdf list "$package" 2>/dev/null | grep -Fq "$version"; then
+      laptop_step_exec \
+        asdf install "$package" "$version" &&
+        asdf set --home "$package" "$version"
+    else
+      laptop_step_exec \
+        asdf set --home "$package" "$version"
+    fi
   else
     laptop_step_exec \
-      asdf set --home "$package" "$version"
+      asdf uninstall "$package" "$version"
   fi
 }
--- src/function/asdf_ensure_plugin.sh
+++ src/function/asdf_ensure_plugin.sh
@@ -3,16 +3,31 @@
 # Install asdf plugin `name` if not present
 #
 # Usage:
-#   laptop_asdf_ensure_plugin <name> <url>
+#   laptop_asdf_ensure_plugin <name> <url> [--status present|absent]
+#
+# Options:
+#   --status present|absent
 #
 laptop_asdf_ensure_plugin() {
   local name="$1"
   local url="$2"
-  laptop_step_start "- Ensure asdf plugin '$name'"
+  # parse options
+  local package_status="present"
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+      -s|--status) package_status="$2"; shift 2;;
+      *) shift;;
+    esac
+  done
+  laptop_step_start "- Ensure asdf plugin '$name' is $package_status"
 
-  if ! asdf plugin list | grep -Fq "$name"; then
-    laptop_step_exec asdf plugin add "$name" "$url"
+  if [ "$package_status" = "present" ]; then
+    if ! asdf plugin list | grep -Fq "$name"; then
+      laptop_step_exec asdf plugin add "$name" "$url"
+    else
+      laptop_step_ok
+    fi
   else
-    laptop_step_ok
+    laptop_step_exec asdf plugin remove "$name"
   fi
 }
