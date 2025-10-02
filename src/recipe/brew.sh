276758ea3423a7a5fa874444dbb3bd4dbb480129
Julien Polo <julien.polo@gmail.com>
2025-10-02 11:25:11 +0200

    ✨ Refactor laptop_package_ensure__brew function to improve resource status handling and enhance clarity for Homebrew package manager management

--- src/recipe/brew.sh
+++ src/recipe/brew.sh
@@ -2,15 +2,22 @@
 
 laptop_package_ensure__brew() {
   # Install Homebrew
-  local brew_present
-  brew_present=$(env -i zsh --login -c 'command -v brew')
-  laptop_step_start_status "present" "unknown" "Package manager 'brew'"
+  local resource_status="present"
+  local current_resource_status
+  current_resource_status=$(env -i zsh --login -c 'command -v brew' &>/dev/null && echo "present" || echo "absent")
+  local message="Package manager 'brew'"
 
-  if [ -z "$brew_present" ]; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &&
-      # eval "$(/opt/homebrew/bin/brew shellenv)" && \;
-      laptop_step_ok
-  else
+  laptop_step_start_status "$resource_status" "$current_resource_status" "$message"
+
+  if [ "$current_resource_status" = "$resource_status" ]; then
     laptop_step_ok
+  else
+    if [ "$resource_status" = "present" ]; then
+      /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &&
+        # eval "$(/opt/homebrew/bin/brew shellenv)" && \;
+        laptop_step_ok || laptop_step_fail
+    else
+      laptop_step_fail
+    fi
   fi
 }
