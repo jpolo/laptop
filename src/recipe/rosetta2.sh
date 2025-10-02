d7a423e0eb7b756a5304fb25d4e749026faa78d6
Julien Polo <julien.polo@gmail.com>
2025-10-02 11:10:35 +0200

    ✨ Refactor laptop_package_ensure__rosetta2 function to improve resource status handling and enhance clarity for Rosetta 2 installation management

--- src/recipe/rosetta2.sh
+++ src/recipe/rosetta2.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 
 laptop_package_ensure__rosetta2() {
+  local resource_status="present"
+
+  local current_resource_status
+  current_resource_status=$(test -f /Library/Apple/usr/share/rosetta/rosetta && echo "present" || echo "absent")
+  local message="Rosetta 2"
+
+  laptop_step_start_status "$resource_status" "$current_resource_status" "$message"
+
   # Install Rosetta
-  laptop_step_start_status "present" "unknown" "Rosetta 2"
-  if ! test arm64 = "$(uname -m)"; then
+  if ! test arm64 = "$(uname -m)" || ! laptop_command_exists "softwareupdate"; then
     laptop_step_pass
-  elif ! laptop_command_exists "softwareupdate"; then
-    laptop_step_pass
-  elif ! test -f /Library/Apple/usr/share/rosetta/rosetta; then
-    laptop_step_exec sudo softwareupdate --install-rosetta --agree-to-license
   else
-    laptop_step_ok
+    if [ "$current_resource_status" = "$resource_status" ]; then
+      laptop_step_ok
+    else
+      laptop_step_exec sudo softwareupdate --install-rosetta --agree-to-license
+    fi
   fi
 }
