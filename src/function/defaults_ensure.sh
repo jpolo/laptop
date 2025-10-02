d3edc8545c97cc97c9d2221a88bbc8c34caa3baa
Julien Polo <julien.polo@gmail.com>
2025-10-02 10:44:15 +0200

    ✨ Refactor laptop_defaults_ensure function to improve resource status handling and enhance clarity for defaults management

--- src/function/defaults_ensure.sh
+++ src/function/defaults_ensure.sh
@@ -6,10 +6,32 @@
 #   laptop_defaults_ensure <domain> <key> <value>
 #
 laptop_defaults_ensure() {
-  laptop_step_start_status "present" "unknown" "Defaults $*"
-  if laptop_command_exists "defaults"; then
-    laptop_step_exec defaults write "$*"
+  local domain="$1"
+  local key="$2"
+  # rest of the arguments
+  local value_type
+  local value
+  local resource_status="present"
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+      -s|--status) resource_status="$2"; shift 2;;
+      -*) value_type="$1"; value="$2"; shift 2;;
+      *) shift;;
+    esac
+  done
+
+  local current_resource_status
+  current_resource_status=$(defaults read "$domain" "$key" &>/dev/null && echo "present" || echo "absent")
+  local message="Defaults ${domain}[${key}] $value_type $value"
+  laptop_step_start_status "$resource_status" "$current_resource_status" "$message"
+
+  if [ "$current_resource_status" = "$resource_status" ]; then
+    laptop_step_ok
   else
-    laptop_step_pass
+    if [ "$resource_status" = "present" ]; then
+      laptop_step_exec defaults write "$domain" "$key" "$value_type" "$value"
+    else
+      laptop_step_exec defaults delete "$domain" "$key"
+    fi
   fi
 }
