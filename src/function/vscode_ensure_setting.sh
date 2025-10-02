0d6b24ca68362a281d99b9e943d9afa674bb2efe
Julien Polo <julien.polo@gmail.com>
2025-10-02 18:22:23 +0200

    ✨ Refactor laptop_vscode_ensure_setting function to enhance resource status handling and improve clarity for VSCode setting management with added support for status options.

--- src/function/vscode_ensure_setting.sh
+++ src/function/vscode_ensure_setting.sh
@@ -3,7 +3,10 @@
 # Ensure VSCode setting
 #
 # Usage:
-#   laptop_vscode_ensure_setting <json_path> <json_value>
+#   laptop_vscode_ensure_setting <json_path> <json_value> [--status present|absent]
+#
+# Options:
+#   --status present|absent
 #
 laptop_vscode_ensure_setting() {
   local executable="${LAPTOP_VSCODE_EXECUTABLE:-code}"
@@ -11,13 +14,27 @@ laptop_vscode_ensure_setting() {
   local app_name
   app_name=$(laptop_vscode_app_name "$executable")
 
-  local json_path="$1"
+  local json_path="$1";
   local json_value="$2"
+  # parse options
+  local resource_status="present"
+  if [ -z "$json_value" ]; then
+    resource_status="absent"
+  fi
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+      -s|--status) resource_status="$2"; shift 2;;
+      *) shift;;
+    esac
+  done
   local vscode_settings_file=""
   local jsonc_args
-  jsonc_args="-v $(quote "$json_value")"
-  if [ "$json_value" = "" ]; then
-    jsonc_args="--delete"
+
+  if [ "$resource_status" = "absent" ]; then
+    jsonc_args+=" --delete"
+    json_value=""
+  else
+    jsonc_args+="-v $(quote "$json_value")"
   fi
 
   local app_dir="Code"
@@ -37,13 +54,31 @@ laptop_vscode_ensure_setting() {
 
   # Vérifier si la requête est vide
   if [ -z "$json_path" ]; then
-    laptop_error "La requête est vide."
+    laptop_error "Empty json_path"
     return 1
   fi
 
-  laptop_step_start_status "present" "unknown" "$app_name setting $json_path=$json_value"
-  laptop_step_eval "\
-  cat $(quote "$vscode_settings_file") | \
-  jsonc modify -n -m -p $(quote "$json_path") $jsonc_args -f $(quote "$vscode_settings_file") \
-  "
+  local current_value
+  current_value="$(cat "$vscode_settings_file" | jsonc read "$json_path" 2>/dev/null)"
+
+  local current_resource_status
+  if [ "$resource_status" = "absent" ]; then
+    current_resource_status=$([ ! -z "$current_value" ] && echo "present" || echo "absent")
+  else
+    current_resource_status=$([ "$current_value" = "$json_value" ] && echo "present" || echo "absent")
+  fi
+  local message
+  message="$app_name setting $json_path=$json_value"
+
+  laptop_step_start_status "$resource_status" "$current_resource_status" "$message"
+
+  if [ "$current_resource_status" = "$resource_status" ]; then
+    laptop_step_ok
+  else
+    if [ "$resource_status" = "present" ]; then
+      laptop_step_eval "cat $(quote $vscode_settings_file) | jsonc modify -n -m -p $(quote "$json_path") $jsonc_args -f $(quote "$vscode_settings_file")"
+    else
+      laptop_step_eval "cat $(quote $vscode_settings_file) | jsonc modify -n -m -p $(quote "$json_path") $jsonc_args -f $(quote "$vscode_settings_file")"
+    fi
+  fi
 }
