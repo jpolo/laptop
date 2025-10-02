a050d858044ae51902a2535cdf92030cf3eda6c5
Julien Polo <julien.polo@gmail.com>
2025-10-02 23:46:45 +0200

    🐛 Fix warnings on linux

--- src/function/configure_steps.sh
+++ src/function/configure_steps.sh
@@ -10,7 +10,7 @@ laptop_configure_steps() {
   steps_dir=${1:-"$(laptop_profile_dir)/steps"}
   local shell=zsh
   local script_files
-  script_files=$(find "$steps_dir" -name '*.sh' -o -name "*.zsh" -maxdepth 1 -mindepth 1 -type f | sort)
+  script_files=$(find "$steps_dir" -maxdepth 1 -mindepth 1 -name '*.sh' -o -name "*.zsh" -type f | sort)
 
   for script_file in $script_files; do
     laptop_shell_exec "$shell" "$script_file"
