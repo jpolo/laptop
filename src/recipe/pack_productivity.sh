970915872834e4762b018ea72f2fa11e7e8ac991
Julien Polo <julien.polo@gmail.com>
2025-10-20 11:34:06 +0200

    ✨ Add hiddenbar to productivity package installation in pack_productivity.sh

--- src/recipe/pack_productivity.sh
+++ src/recipe/pack_productivity.sh
@@ -5,6 +5,7 @@ laptop_package_ensure__pack:productivity() {
   laptop_package_ensure "ollama"
   laptop_package_ensure "drawio"
   laptop_package_ensure "notion"
+  laptop_package_ensure "hiddenbar"
   # laptop_package_ensure "rectangle"
   if [[ ${LAPTOP_PROFILE_PRIVACY:-strict} = strict ]];then
     laptop_package_ensure "vivaldi"
