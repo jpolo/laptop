ac138dbe7709f5e8cf29a905a50b93f4dce41a59
Julien Polo <julien.polo@gmail.com>
2025-10-22 10:51:32 +0200

    ✨ Add npm_ensure_package_list function to install npm packages from a specified file

---
+++ src/function/npm_ensure_package_list.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Install all npm packages from "npmfile" like file
+#
+# Usage:
+#   laptop_npm_ensure_package_list <npm_file>
+#
+laptop_npm_ensure_package_list() {
+  local npm_file="$1"
+  local target_package
+
+  # Check if the file exists
+  if [[ -f "$npm_file" ]]; then
+    # Read the file line by line
+    while IFS= read -r line; do
+      target_package="$line"
+
+      # If the language matches, return the version
+      laptop_npm_ensure_package "$target_package"
+    done < "$npm_file"
+  else
+    laptop_warn "$npm_file does not exist"
+  fi
+}
