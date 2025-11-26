d721435baff63b22bc8a3a3c435c6d0efab01c22
Julien Polo <julien.polo@gmail.com>
2025-11-26 18:54:36 +0100

    ➖ Comment out 'openshot-video-editor' package as it is not currently useful

--- lib/recipe/pack_media.sh
+++ lib/recipe/pack_media.sh
@@ -5,5 +5,5 @@ laptop_package_ensure__pack:media() {
   laptop_package_ensure "excalidrawz"
   laptop_package_ensure "inkscape"
   laptop_package_ensure "gimp"
-  laptop_package_ensure "openshot-video-editor"
+  # laptop_package_ensure "openshot-video-editor" Not really useful at the moment
 }
