a6ce53a37c93614120f9a5b98009a38b306f0adc
Julien Polo <julien.polo@gmail.com>
2025-12-01 18:38:59 +0100

    ✅ Add assert.sh and assert_snapshot.sh for enhanced bash unit testing capabilities

---
+++ test/assert_snapshot.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Example
+#
+# assert_snapshot "my-command" "my-command.out"
+assert_snapshot() {
+  local test_command="$1"
+  local snapshot_file;
+  local snapshot_content;
+  snapshot_file="$TEST_DIR/__snapshots__/$2"
+
+  if [[ "$UPDATE_SNAPSHOT" != "" || ! -f $snapshot_file ]];then
+    mkdir -p "$(dirname "$snapshot_file")"
+    eval "$test_command" > "$snapshot_file"
+  fi
+  snapshot_content=$(cat "$snapshot_file")
+
+  assert "$test_command" "$snapshot_content"
+}
