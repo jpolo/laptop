a5f225a049ecde6a91bcf11f9c11227b0b65021d
Julien Polo <julien.polo@gmail.com>
2025-12-05 18:20:58 +0100

    ✅ Refactor file_ensure tests to use assertions for better clarity and reliability

--- test/file_ensure.test.sh
+++ test/file_ensure.test.sh
@@ -8,5 +8,7 @@ assert_raises "laptop_directory_ensure '$TMPDIR/folder_create/'" 0
 assert "file -b $TMPDIR/folder_create/" "directory"
 
 # Test laptop_file_ensure
-laptop_file_ensure "$TMPDIR/test_file_create"
-laptop_file_ensure "$TMPDIR/test_file_create"
+assert_raises "laptop_file_ensure '$TMPDIR/test_file_create'" 0
+assert "file -b $TMPDIR/test_file_create" "empty"
+assert_raises "laptop_file_ensure '$TMPDIR/test_file_create'" 0
+assert "file -b $TMPDIR/test_file_create" "empty"
