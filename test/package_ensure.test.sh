f55785c0dc1c41483cfac017f462e4c00f12f834
Julien Polo <julien.polo@gmail.com>
2025-12-05 18:46:19 +0100

    ✅ Update package_ensure tests to reflect changes in brew and asdf command assertions

--- test/package_ensure.test.sh
+++ test/package_ensure.test.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # Try to run brew installation
-assert_raises "command -v brew" 0
+assert_raises "command -v brew" 1
 assert_raises "laptop_package_ensure 'brew'" 0
 assert_raises "command -v brew" 0
 
 # Test laptop_package_ensure
-assert_raises "command -v asdf" 0
+assert_raises "command -v asdf" 1
 assert_raises "laptop_package_ensure 'asdf'" 0
 assert_raises "command -v asdf" 0
 assert_raises "laptop_package_ensure 'asdf'" 0
