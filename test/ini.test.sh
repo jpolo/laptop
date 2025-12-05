e5d62e1d7c34d4841ca8416aedaa56f44d9cbd02
Julien Polo <julien.polo@gmail.com>
2025-12-05 18:28:55 +0100

    ✅ Add ini_test.ini and ini.test.sh for testing laptop_ini_get functionality

---
+++ test/ini.test.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Test laptop_ini_get
+
+assert "laptop_ini_get '$TEST_DIR/ini_test.ini' 'root'" "root_value"
+# assert "laptop_ini_get '$TEST_DIR/ini_test.ini' 'section.section_key'" "section_value"
+# assert "laptop_ini_get '$TEST_DIR/ini_test.ini' 'section2.section2_key'" "section2_value"
+assert "laptop_ini_get '$TEST_DIR/ini_test.ini' 'unknown_key'" ""
