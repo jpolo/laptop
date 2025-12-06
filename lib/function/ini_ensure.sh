06eb307f3a25865b78bac0fd1174c5651e5b067d
Julien Polo <julien.polo@gmail.com>
2025-12-06 22:00:40 +0100

    ✨ Add laptop_ini_ensure function and corresponding tests for INI file management

---
+++ lib/function/ini_ensure.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+# Set INI file values
+#
+# Usage:
+#   laptop_ini_ensure <file> <key> <value>
+#
+laptop_ini_ensure() {
+  local file="$1"
+  local section=".anon"
+  local key="$2"
+  local value="$3"
+
+  # split section if key contains a dot
+  if [[ "$key" == *.* ]]; then
+    section=$(echo "$key" | cut -d'.' -f1)
+    key=$(echo "$key" | cut -d'.' -f2)
+  fi
+
+  local current_value
+  current_value=$(laptop_ini_get "$file" "$section.${key}")
+
+  local resource_status="present"
+  local current_resource_status
+  current_resource_status=$(echo "$current_value" | grep -q "$value" && echo "present" || echo "absent")
+
+  local resource_status="present"
+  local current_resource_status
+
+  local message
+  message="INI setting [\"$section\"][\"$key\"]='$value'"
+
+  laptop_step_start_status "$resource_status" "$current_resource_status" "$message"
+
+  if [ "$current_resource_status" = "$resource_status" ]; then
+    laptop_step_ok
+  else
+    if [ "$resource_status" = "present" ]; then
+      laptop_step_exec augtool -A <<EOF
+set /augeas/load/IniFile/lens IniFile.lns_loose
+set /augeas/load/IniFile/incl '$file'
+load
+set "/files/$file/section[.=$(quote "$section")]" $(quote "$section")
+set "/files/$file/section[.=$(quote "$section")]/${key}" $(quote "$value")
+save
+EOF
+    else
+      laptop_step_fail
+      laptop_error "Not implemented"
+    fi
+  fi
+}
+
+
+
+
+
+
--- test/ini.test.sh
+++ test/ini.test.sh
@@ -6,3 +6,13 @@ assert "laptop_ini_get '$TEST_DIR/ini_test.ini' 'root'" "root_value"
 assert "laptop_ini_get '$TEST_DIR/ini_test.ini' 'section.section_key'" "section_value"
 assert "laptop_ini_get '$TEST_DIR/ini_test.ini' 'section2.section2_key'" "section2_value"
 assert "laptop_ini_get '$TEST_DIR/ini_test.ini' 'unknown_key'" ""
+
+# Test laptop_ini_ensure
+ensure_ini_file="$TMPDIR/ini_ensure.ini"
+assert_raises "laptop_file_ensure '$ensure_ini_file'" 0
+assert_raises "laptop_ini_ensure '$ensure_ini_file' 'root' 'root_value'" 0
+assert_raises "laptop_ini_ensure '$ensure_ini_file' 'root_overwrite' 'root_overwrite_initial'" 0
+assert_raises "laptop_ini_ensure '$ensure_ini_file' 'root_overwrite' 'root_overwrite_next'" 0
+assert_raises "laptop_ini_ensure '$ensure_ini_file' 'section.new_key' 'value_initial'" 0
+assert_raises "laptop_ini_ensure '$ensure_ini_file' 'section.new_key' 'value_next'" 0
+assert_snapshot "cat '$ensure_ini_file'" "ini_ensure.ini.snap"
