27fdf38a63812a8fd69e0e6daa3c1c5219e2bff7
Julien Polo <julien.polo@gmail.com>
2025-12-08 00:36:52 +0100

    🐛 Move laptop_asdf_ensure_plugin and laptop_asdf_ensure_package tests to package_ensure.test.sh

--- test/asdf.test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Test laptop_asdf_ensure_plugin
-assert_raises "laptop_asdf_ensure_plugin 'nodejs' 'https://github.com/asdf-vm/asdf-nodejs.git'" 0
-assert_raises "laptop_asdf_ensure_plugin 'nodejs' 'https://github.com/asdf-vm/asdf-nodejs.git'" 0
-
-# Test laptop_asdf_ensure_package
-assert_raises "laptop_asdf_ensure_package 'nodejs' 'latest:20'" 0
-assert_raises "laptop_asdf_ensure_package 'nodejs' 'latest:20'" 0
--- test/package_ensure.test.sh
+++ test/package_ensure.test.sh
@@ -8,3 +8,11 @@ assert_raises "command -v brew" 0
 assert_raises "laptop_package_ensure 'asdf'" 0
 assert_raises "command -v asdf" 0
 assert_raises "laptop_package_ensure 'asdf'" 0
+
+# Test laptop_asdf_ensure_plugin
+assert_raises "laptop_asdf_ensure_plugin 'nodejs' 'https://github.com/asdf-vm/asdf-nodejs.git'" 0
+assert_raises "laptop_asdf_ensure_plugin 'nodejs' 'https://github.com/asdf-vm/asdf-nodejs.git'" 0
+
+# Test laptop_asdf_ensure_package
+assert_raises "laptop_asdf_ensure_package 'nodejs' 'latest:20'" 0
+assert_raises "laptop_asdf_ensure_package 'nodejs' 'latest:20'" 0
