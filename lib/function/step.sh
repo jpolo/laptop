596a818600266975ee78ad408ce9dbea22be1e12
Julien Polo <julien.polo@gmail.com>
2025-12-08 15:38:12 +0100

    🔧 Refactor LAPTOP_STEP_STATUS_COLUMN and SET_COL variables to use underscore prefix for consistency

--- lib/function/step.sh
+++ lib/function/step.sh
@@ -4,8 +4,8 @@ laptop_step_start() {
   local message="$1"
   # display the message and truncate the line to $COLUMNS characters
   # and adds a ... at the end if truncated
-  if [ "${#message}" -ge "$LAPTOP_STEP_STATUS_COLUMN" ]; then
-    message="${message:0:$LAPTOP_STEP_STATUS_COLUMN-5}..."
+  if [ "${#message}" -ge "$_LAPTOP_STEP_STATUS_COLUMN" ]; then
+    message="${message:0:$_LAPTOP_STEP_STATUS_COLUMN-5}..."
   fi
   echo -n -e "$message"
   return 0
@@ -50,17 +50,17 @@ laptop_step_upgrade_start() {
 }
 
 laptop_step_ok() {
-  echo -e "${SET_COL}${BRACKET}[${SUCCESS}  OK  ${BRACKET}]${NORMAL}"
+  echo -e "${_LAPTOP_SET_COL}${BRACKET}[${SUCCESS}  OK  ${BRACKET}]${NORMAL}"
   return 0
 }
 
 laptop_step_fail() {
-  echo -e "${SET_COL}${BRACKET}[${FAILURE} FAIL ${BRACKET}]${NORMAL}"
+  echo -e "${_LAPTOP_SET_COL}${BRACKET}[${FAILURE} FAIL ${BRACKET}]${NORMAL}"
   return 0
 }
 
 laptop_step_pass() {
-  echo -e "${SET_COL}${BRACKET}[${NORMAL} PASS ${BRACKET}]${NORMAL}"
+  echo -e "${_LAPTOP_SET_COL}${BRACKET}[${NORMAL} PASS ${BRACKET}]${NORMAL}"
   return 0
 }
 
