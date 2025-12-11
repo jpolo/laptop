b0671f6cbef07104178b34cd53efe03e8204fb50
Julien Polo <julien.polo@gmail.com>
2025-12-11 17:06:18 +0100

    📝 Add usage documentation for laptop_die function to clarify error handling

--- lib/function/die.sh
+++ lib/function/die.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+# Stop execution with a message
+#
+# Usage:
+#   laptop_die <message>
+#
+# Example:
+#   laptop_die "This is an error message"
+#
 laptop_die() {
   laptop_log error "$1"
   exit 1
