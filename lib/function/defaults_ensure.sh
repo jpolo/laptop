2e81f278ed44b63150549551fc844bee78f35ce5
Julien Polo <julien.polo@gmail.com>
2025-12-12 13:22:26 +0100

    🐛 Fix defaults_ensure bug when setting a non empty value

--- lib/function/defaults_ensure.sh
+++ lib/function/defaults_ensure.sh
@@ -33,8 +33,16 @@ laptop_defaults_ensure() {
     esac
   done
 
+  local current_value
+  current_value=$(defaults read "$domain" "$key" "$value_type" 2>/dev/null)
+  case "$value_type" in
+    -bool)
+      current_value=$([ "$current_value" = "0" ] && echo "false" || echo "true")
+      ;;
+  esac
+
   local current_resource_status
-  current_resource_status=$(defaults read "$domain" "$key" &>/dev/null && echo "present" || echo "absent")
+  current_resource_status=$([ "$current_value" = "$value" ] && echo "present" || echo "absent")
   local message="Defaults ${domain}[${key}] $value_type $value"
 
   laptop_step_start_status "$resource_status" "$current_resource_status" "$message"
