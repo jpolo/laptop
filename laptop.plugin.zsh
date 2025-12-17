6dde5aa3457a6ec18da021e508c9e0558f3458a0
Julien Polo <julien.polo@gmail.com>
2025-12-17 22:23:32 +0100

    ✨ Add LAPTOP_INSTALL_METHOD variable to Makefile and laptop scripts for improved installation method configuration

--- laptop.plugin.zsh
+++ laptop.plugin.zsh
@@ -3,3 +3,6 @@
 
 # Add the bin directory to your PATH
 export PATH="$PATH:$(dirname $(realpath $0))/bin"
+
+# Set the default installation method for laptop plugins
+export LAPTOP_INSTALL_METHOD=${LAPTOP_INSTALL_METHOD:-"zinit"}
