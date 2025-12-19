a68e0aa405f6b700e8ebdd7c77037894a9767a73
Julien Polo <julien.polo@gmail.com>
2025-12-19 16:04:34 +0100

    ♻️ Refactor variable management: replace shell_profile_var with shell_ensure_var for improved variable handling in setup_default_shell.sh

--- lib/function/setup_default_shell.sh
+++ lib/function/setup_default_shell.sh
@@ -2,7 +2,7 @@
 
 laptop_require "laptop_profile_dir"
 laptop_require "laptop_file_ensure_template"
-laptop_require "laptop_shell_profile_var"
+laptop_require "laptop_shell_ensure_var"
 laptop_require "laptop_shell_exec"
 
 # Configure default profile (.profile, .zprofile, .zshrc, etc) using default profile templates
@@ -14,7 +14,7 @@ laptop_setup_default_shell() {
   local profile_dir
   profile_dir=$(laptop_profile_dir default)
 
-  laptop_shell_profile_var "LAPTOP_GIT_REMOTE" "${LAPTOP_GIT_REMOTE}"
+  laptop_shell_ensure_var "$HOME/.profile" "LAPTOP_GIT_REMOTE" "${LAPTOP_GIT_REMOTE}"
 
   laptop_file_ensure_template "$profile_dir/resource/.zshrc" "$HOME/.zshrc" --force
   laptop_file_ensure_template "$profile_dir/resource/.zshrc.local" "$HOME/.zshrc.local"
---
+++ lib/function/shell_ensure_var.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+laptop_require "laptop_step_start_status"
+laptop_require "laptop_step_eval"
+laptop_require "laptop_step_status"
+laptop_require "laptop_log"
+laptop_require "laptop_path_print"
+laptop_require "laptop_file_var"
+
+#
+# Add or Update variable expression in script file
+#
+# Usage:
+#   laptop_shell_ensure_var <script_file> <var_name> <value>
+#
+laptop_shell_ensure_var() {
+  local script_file="$1"
+  local var_name="$2"
+  local value="$3"
+
+  local resource_status="present"
+
+  local resource_current_status="unknown"
+  laptop_step_start_status "$resource_status" "$resource_current_status" "Variable '$var_name=$value' in '$(laptop_path_print "$script_file")'"
+
+  if [ "$resource_current_status" = "$resource_status" ]; then
+    laptop_step_status "ok"
+  else
+    if [ "$resource_status" = "present" ]; then
+      if [ -n "$value" ]; then
+        laptop_step_exec laptop_file_var "$script_file" "$var_name" "$value"
+      else
+        laptop_step_status "pass"
+        laptop_log warn "$var_name is empty in '$(laptop_path_print "$script_file")'"
+      fi
+    else
+      laptop_step_exec laptop_die "Not Implemented"
+    fi
+  fi
+}
