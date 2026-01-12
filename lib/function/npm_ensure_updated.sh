b44d3723629d164bca1414faa64bb8e287b07a42
Julien Polo <julien.polo@gmail.com>
2026-01-12 12:51:12 +0100

    ✨ Add npm_ensure_updated script to ensure global npm packages are up to date

---
+++ lib/function/npm_ensure_updated.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+laptop_require "laptop_step_upgrade_start"
+laptop_require "laptop_step_eval"
+
+# Ensure npm packages are up to date
+#
+# Usage:
+#   laptop_npm_ensure_updated
+#
+laptop_npm_ensure_updated() {
+  laptop_step_upgrade_start "npm updated"
+  laptop_step_eval "npm install -g"
+}
