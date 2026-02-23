9c888ecf51bd4e48c6f5d548139789cf14c427a7
Julien Polo <julien.polo@gmail.com>
2026-02-23 14:21:45 +0100

    ✨ Add confirmation before self-upgrade

--- lib/command/self-upgrade.sh
+++ lib/command/self-upgrade.sh
@@ -3,10 +3,21 @@
 laptop_require "laptop_log"
 laptop_require "laptop_self_ensure_updated"
 
+laptop_command__self-upgrade() {
+  laptop_handler_call "logo"
+  if laptop_confirm "Continue? (y/N)"; then
+    laptop_command__self-upgrade_run
+
+    laptop_log info "🎉 Self upgrade finished"
+  else
+    laptop_die "🛑 Upgrade aborted"
+  fi
+}
+
 # Upgrade only the laptop CLI (when installed via Homebrew).
 # When installed via git/zinit, use 'laptop upgrade' to update from the remote.
 #
-laptop_command__self-upgrade() {
+laptop_command__self-upgrade_run() {
   if [ -z "$LAPTOP_INSTALL_BREW_PACKAGE" ] || ! brew list "$LAPTOP_INSTALL_BREW_PACKAGE" &>/dev/null; then
     laptop_log warn "laptop is not installed via Homebrew ($LAPTOP_INSTALL_BREW_PACKAGE). Please run in your terminal :\n\n\
     brew install $LAPTOP_INSTALL_BREW_PACKAGE\n\n"
@@ -14,5 +25,4 @@ laptop_command__self-upgrade() {
   fi
 
   laptop_self_ensure_updated
-  laptop_log info "🎉 laptop self-upgrade finished"
 }
