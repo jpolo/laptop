d895f1763742eecd8fa464376b739d483cbc693c
Julien Polo <julien.polo@gmail.com>
2026-03-16 22:02:31 +0100

    ✨ Enable LAPTOP_BREW_AUTOUPDATE for default profile

--- profile/default/profile.sh
+++ profile/default/profile.sh
@@ -5,6 +5,8 @@ LAPTOP_PROFILE_PRIVACY="${LAPTOP_PROFILE_PRIVACY:-strict}"
 # shellcheck disable=SC2034
 LAPTOP_INSTALL_BREW_PACKAGE="w5s/tap/laptop"
 
+LAPTOP_BREW_AUTOUPDATE=${LAPTOP_BREW_AUTOUPDATE:-"false"}
+
 # Handlers
 # Uncomment to override the default handler
 
