40b0d84e814f143bc43cbcac746cce661ac73cbf
Julien Polo <julien.polo@gmail.com>
2026-06-12 14:27:26 +0200

    ✨ Update MongoDB package paths in installation scripts for brew compatibility

--- lib/package/mongodb-community.sh
+++ lib/package/mongodb-community.sh
@@ -2,12 +2,10 @@
 
 laptop_require "laptop_package_ensure_default"
 laptop_require "laptop_brew_ensure_package"
-laptop_require "laptop_brew_ensure_tap"
 
 laptop_package_ensure__mongodb-community() {
   if [ "$LAPTOP_PACKAGE_MANAGER" = "brew" ]; then
-    laptop_brew_ensure_tap "mongodb/brew"
-    laptop_brew_ensure_package "mongodb-community@6.0" "$@"
+    laptop_brew_ensure_package "mongodb/brew/mongodb-community@6.0" "$@"
   else
     laptop_package_ensure_default "mongodb-community" "$@"
   fi
--- lib/package/mongodb-database-tools.sh
+++ lib/package/mongodb-database-tools.sh
@@ -2,12 +2,10 @@
 
 laptop_require "laptop_package_ensure_default"
 laptop_require "laptop_brew_ensure_package"
-laptop_require "laptop_brew_ensure_tap"
 
 laptop_package_ensure__mongodb-database-tools() {
   if [ "$LAPTOP_PACKAGE_MANAGER" = "brew" ]; then
-    laptop_brew_ensure_tap "mongodb/brew"
-    laptop_brew_ensure_package "mongodb-database-tools"
+    laptop_brew_ensure_package "mongodb/brew/mongodb-database-tools"
   else
     laptop_package_ensure_default "mongodb-database-tools"
   fi
