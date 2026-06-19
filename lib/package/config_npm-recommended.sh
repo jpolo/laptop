247120b20d6b96babff37318447383dd616c4964
Julien Polo <julien.polo@gmail.com>
2026-06-19 14:49:56 +0000

    🐛 Remove NPM_TOKEN configuration

--- lib/package/config_npm-recommended.sh
+++ lib/package/config_npm-recommended.sh
@@ -3,8 +3,9 @@
 laptop_require "laptop_npm_config_ensure"
 
 laptop_package_ensure__config:npm-recommended() {
+  # Login using GITHUB_TOKEN to npm github npm.pkg.github.com 
+  laptop_shell_ensure_var "$HOME/.profile" "GITHUB_TOKEN" '"\$(GITHUB_TOKEN= gh auth token 2>/dev/null)"'
 
   # Keep variable references in .npmrc so token updates do not require rewrites.
   laptop_npm_config_ensure "//npm.pkg.github.com/:_authToken" '${GITHUB_TOKEN}'
-  laptop_npm_config_ensure "//registry.npmjs.org/:_authToken" '${NPM_TOKEN}'
 }
