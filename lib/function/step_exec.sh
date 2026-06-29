9531fb36f12e72a28da5adec897034fad7d9cf76
Julien Polo <julien.polo@gmail.com>
2026-06-29 18:55:29 +0200

    ✨ Refactor laptop_step_exec to capture command output and log errors

--- lib/function/step_exec.sh
+++ lib/function/step_exec.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-laptop_require "laptop_step_eval"
+laptop_require "laptop_step_start_status"
+laptop_require "laptop_step_status"
+laptop_require "laptop_log"
 
 # Execute a command and display the output
 #
@@ -8,5 +10,16 @@ laptop_require "laptop_step_eval"
 #   laptop_step_exec <command> <args...>
 #
 laptop_step_exec() {
-  laptop_step_eval "$*"
+  local output
+  output=$("$@" 2>&1)
+  local exit_code=$?
+
+  if [ "$exit_code" = "0" ]; then
+    laptop_step_status "ok"
+  else
+    laptop_step_status "fail"
+    laptop_log error "Command failed \
+      \\n|  > $* \
+      \\n|  $output"
+  fi
 }
