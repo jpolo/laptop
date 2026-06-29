9c850b89c8b2d01bbe37c4f1ea7153627e367538
Julien Polo <julien.polo@gmail.com>
2026-06-29 16:07:53 +0200

    🐛 Remove some uneeded dependencies

--- lib/package/pack_social.sh
+++ lib/package/pack_social.sh
@@ -5,6 +5,6 @@ laptop_require "laptop_package_ensure"
 laptop_package_ensure__pack:social() {
   laptop_package_ensure "discord"
   laptop_package_ensure "slack"
-  laptop_package_ensure "whatsapp"
-  laptop_package_ensure "zoom"
+  # laptop_package_ensure "whatsapp" Not often used for work
+  # laptop_package_ensure "zoom" Not often used for work
 }
