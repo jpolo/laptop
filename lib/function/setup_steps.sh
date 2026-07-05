b8aeb7d54e2c26cba6e3413b6e502abd920ad237
Julien Polo <julien.polo@gmail.com>
2026-07-05 23:59:55 +0200

    ✨ Refactor setup_steps to use laptop_shell_exec_dir_d and add new shell_exec_dir_d function

--- lib/function/setup_steps.sh
+++ lib/function/setup_steps.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 laptop_require "laptop_profile_dir"
-laptop_require "laptop_shell_exec"
+laptop_require "laptop_shell_exec_dir_d"
 
 # Run every steps in alphabetic order (in a separate shell instance)
 #
@@ -11,11 +11,5 @@ laptop_require "laptop_shell_exec"
 laptop_setup_steps() {
   local steps_dir
   steps_dir=${1:-"$(laptop_profile_dir)/steps"}
-  local shell=zsh
-  local script_files
-  script_files=$(find "$steps_dir" -maxdepth 1 -mindepth 1 -name '*.sh' -o -name "*.zsh" -type f | sort)
-
-  for script_file in $script_files; do
-    LAPTOP_SOURCE_ALL=true laptop_shell_exec "$shell" "$script_file"
-  done
+  LAPTOP_SOURCE_ALL=true laptop_shell_exec_dir_d "$steps_dir"
 }
---
+++ lib/function/shell_exec_dir_d.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+laptop_require "laptop_shell_exec"
+
+# Run every shell file in alphabetic order (in a separate shell instance)
+#
+# Usage:
+#   laptop_shell_exec_dir_d <steps_dir>
+#
+laptop_shell_exec_dir_d() {
+  local steps_dir="$1"
+  local shell=zsh
+  local script_files
+  script_files=$(find "$steps_dir" -maxdepth 1 -mindepth 1 -name '*.sh' -o -name "*.zsh" -type f | sort)
+
+  for script_file in $script_files; do
+    laptop_shell_exec "$shell" "$script_file"
+  done
+}
