566856d989d2088c83115e9820b74183a7c082d7
Julien Polo <julien.polo@gmail.com>
2026-07-21 17:55:50 +0200

    🔧 Refactor npm authentication logic by moving GITHUB_TOKEN setup to login script

--- lib/package/config_npm-recommended.sh
+++ lib/package/config_npm-recommended.sh
@@ -3,11 +3,6 @@
 laptop_require "laptop_npm_config_ensure"
 
 laptop_package_ensure__config:npm-recommended() {
-  # Login using GITHUB_TOKEN to npm github npm.pkg.github.com
-  # shellcheck disable=SC2016
-  laptop_shell_ensure_var "$HOME/.profile" "GITHUB_TOKEN" '"$(GITHUB_TOKEN= gh auth token 2>/dev/null)"' --export
-
-  # Keep variable references in .npmrc so token updates do not require rewrites.
-  # shellcheck disable=SC2016
-  laptop_npm_config_ensure "//npm.pkg.github.com/:_authToken" '${GITHUB_TOKEN}'
+  # login logic moved to logic command
+  true
 }
--- profile/default/login.d/0-github.zsh
+++ profile/default/login.d/0-github.zsh
@@ -4,3 +4,11 @@
 # Uses the existing `laptop_github_ensure_login` function.
 
 laptop_github_ensure_login
+
+# Login using GITHUB_TOKEN to npm github npm.pkg.github.com
+# shellcheck disable=SC2016
+laptop_shell_ensure_var "$HOME/.profile" "GITHUB_TOKEN" '"$(GITHUB_TOKEN= gh auth token 2>/dev/null)"' --export
+
+# Keep variable references in .npmrc so token updates do not require rewrites.
+# shellcheck disable=SC2016
+laptop_npm_config_ensure "//npm.pkg.github.com/:_authToken" '${GITHUB_TOKEN}'
