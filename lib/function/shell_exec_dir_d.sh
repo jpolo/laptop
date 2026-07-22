fa64204281215ebbe850cb5813dc9860a01b0675
Julien Polo <julien.polo@gmail.com>
2026-07-22 14:21:00 +0200

    🐛 Fix script file execution to handle empty results and improve readability

--- lib/function/shell_exec_dir_d.sh
+++ lib/function/shell_exec_dir_d.sh
@@ -11,9 +11,9 @@ laptop_shell_exec_dir_d() {
   local steps_dir="$1"
   local shell=zsh
   local script_files
-  script_files=$(find "$steps_dir" -maxdepth 1 -mindepth 1 -name '*.sh' -o -name "*.zsh" -type f | sort)
+  script_files=$(find "$steps_dir" -maxdepth 1 -mindepth 1 \( -name '*.sh' -o -name "*.zsh" \) -type f | sort)
 
-  for script_file in $script_files; do
-    laptop_shell_exec "$shell" "$script_file"
-  done
+  while IFS= read -r script_file; do
+    [[ -n "$script_file" ]] && laptop_shell_exec "$shell" "$script_file"
+  done <<< "$script_files"
 }
