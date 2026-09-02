3951523596813d961d5d67664e05567c1e63b40f
Julien Polo <julien.polo@gmail.com>
2026-09-02 16:08:56 +0200

    🐛 Fix boot_seconds extraction regex in laptop_uptime function for better accuracy

--- lib/function/uptime.sh
+++ lib/function/uptime.sh
@@ -7,7 +7,9 @@ laptop_uptime() {
   local up_seconds
   if [ "$(uname -s)" = "Darwin" ]; then
     local boot_seconds now
-    boot_seconds="$(sysctl -n kern.boottime | sed -E 's/.*sec = ([0-9]+).*/\1/')"
+    # Output looks like: { sec = 1788257275, usec = 196255 } Tue Sep  1 12:07:55 2026
+    # Anchor on the first number so the greedy match does not pick up "usec".
+    boot_seconds="$(sysctl -n kern.boottime | sed -E 's/^[^0-9]*([0-9]+).*/\1/')"
     now="$(laptop_date_to_epoch "$(laptop_date_now)")"
     up_seconds=$((now - boot_seconds))
   else
@@ -15,3 +17,4 @@ laptop_uptime() {
   fi
   echo "$up_seconds"
 }
+
